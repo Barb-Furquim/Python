8a962be38595ba568e1e5140e5cf48477c93aba1
Bárbara Furquim <113610637+Barb-Furquim@users.noreply.github.com>
2022-11-21 12:22:10 -0300

    Create phonebook.py

---
+++ phonebook.py
@@ -0,0 +1,10 @@
+from cs50 import get_string
+
+people = {
+    "Brian": "3481-0056",
+    "David": "99875-8517"
+}
+
+nome = get_string("Nome: ")
+if nome in people:
+    print(f'Number: {people[nome]}')
