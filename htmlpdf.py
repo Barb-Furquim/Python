91d576f4862d3c39a9edae0eaa4823113d5d63ac
Bárbara Furquim <113610637+Barb-Furquim@users.noreply.github.com>
2023-04-24 23:56:47 -0300

    Update htmlpdf.py

--- htmlpdf.py
+++ htmlpdf.py
@@ -1,14 +1,25 @@
-# Project convert file HTML in file PDF
+# script for convert file HTML in file PDF 
+# script in progress
 
 import pdfkit
 from tkinter import *
 
-def covert():
-  
-    pdfkit.from_url("https://www.google.com.br/", "google.pdf")
-    
-convert()
+def html_pdf():
+
+    htmlpdf = pdfkit.from_url("https://www.google.com.br/", "google.pdf")
+
+html_pdf()
 
 janela = Tk()
+janela.title("Converter HTML para PDF")
+
+# criação do texto dentro da janela
+texto_orientacao = Label(janela, text="Clique no botão para selecionar o arquivo HTML: ")
+
+# posição do texto
+texto_orientacao.grid(column=0, row=0)
+
+botao = Button(janela, text="Carregar arquivo HTML", command=html_pdf)
+botao.grid(column=0, row=1)
 
 janela.mainloop()
